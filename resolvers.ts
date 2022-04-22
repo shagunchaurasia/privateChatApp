14fd66df1de6762adbb30410fc8582a169bb02e7
shagunchaurasia <chaurasia.shagun@gmail.com>
2022-04-22 20:44:44 +0900

    jwt token added and sign in and sign up logic added

--- resolvers.ts
+++ resolvers.ts
@@ -1,5 +1,56 @@
+import { AuthenticationError } from 'apollo-server';
+import {PrismaClient} from "@prisma/client";
+import bcrypt from 'bcryptjs';
+import jwt from 'jsonwebtoken';
+
+
+const prisma = new PrismaClient();
+
 const resolvers = {
   Query: {},
+ 
+  Mutation: {
+    signUpUser: async(parent: any, args: any, context: any) => {
+        const {userNew} = args;
+      const user  = await prisma.user.findUnique({ where: { email: userNew.email } });
+      if(user){
+          throw new AuthenticationError("User already exists")
+      }
+
+      const passwordHashed = await bcrypt.hash(userNew.password, 10);
+
+     const newUser =  await prisma.user.create({
+        data: {
+          ...userNew,
+          password: passwordHashed
+        },
+      });
+
+      return newUser;
+    },
+    signInUser: async(parent: any, args: any, context: any)=>{
+        const {userDetails} = args;
+      const user = await prisma.user.findUnique({
+        where: { email: userDetails.email },
+      });
+
+      if(!user){
+          throw new AuthenticationError("User doesn't exist with the provided email")
+      }
+    const passwordMatch =  await bcrypt.compare(userDetails.password, user.password);
+
+    if(!passwordMatch){
+        throw new AuthenticationError("Email or Password invalid")
+    }
+    const JWT_SECRET = process.env.JWT_SECRET!;
+    const token = jwt.sign({userId: user.id}, JWT_SECRET)
+    return {token};
+
+      
+ 
+
+    }
+  },
 };
 
 export default resolvers;
--- typeDefs.ts
+++ typeDefs.ts
@@ -2,10 +2,33 @@ import {gql} from 'apollo-server';
 
 const typeDefs = gql`
   type Query {
-    greet: String
+    users: [User]
+    user(id: ID!): User
   }
 
-
+  input UserInput {
+    firstName: String!
+    lastName: String!
+    email: String!
+    password: String!
+  }
+  input UserSignInInput {
+    email: String!
+    password: String!
+  }
+  type Token{
+    token: String!
+  }
+  type Mutation {
+    signUpUser(userNew: UserInput!): User
+    signInUser(userDetails: UserSignInInput!): Token
+  }
+  type User {
+    id: ID!
+    firstName: String!
+    lastName: String!
+    email: String!
+  }
 `;
 
 
